f93c74b67bf451c3eb64c4ee94c9c18d5218506f
akshayvpdev <akshayvpofficial@gmail.com>
2025-04-17 23:13:38 +0530

    new commit

--- models/Design.js
+++ models/Design.js
@@ -2,23 +2,23 @@
 
 const mongoose = require("mongoose");
 
-const styleSchema = new mongoose.Schema({
-  x: { type: Number, required: true },
-  y: { type: Number, required: true },
-  width: { type: Number, required: true },
-  height: { type: Number, required: true },
-  rotation: { type: Number, required: true },
-  fontSize: { type: Number },
-  shapeType: { type: String, required: true },
-  color: { type: String },
-  backgroundColor: { type: String },
-});
+// const styleSchema = new mongoose.Schema({
+//   x: { type: Number, required: true },
+//   y: { type: Number, required: true },
+//   width: { type: Number, required: true },
+//   height: { type: Number, required: true },
+//   rotation: { type: Number, required: true },
+//   fontSize: { type: Number },
+//   shapeType: { type: String, required: true },
+//   color: { type: String },
+//   backgroundColor: { type: String },
+// });
 
 const elementSchema = new mongoose.Schema({
   id: { type: String, required: true },
   type: { type: String, enum: ["text", "image", "shape"], required: true },
   content: { type: String},
-  style: { type: styleSchema, required: true },
+  style: { type: mongoose.Schema.Types.Mixed, required: true },
 });
 
 const canvasSizeSchema = new mongoose.Schema({
