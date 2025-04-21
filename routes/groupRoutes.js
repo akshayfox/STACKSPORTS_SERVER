bbec7f4a94df392583ce5807246ee814fa6b6ecf
akshayvpdev <akshayvpofficial@gmail.com>
2025-04-21 08:52:39 +0530

    new commit

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -9,23 +9,11 @@ const {
 const { verifyToken } = require('../controllers/authController');
 
 const router = express.Router();
-
-// Apply authentication middleware to all group routes
 router.use(verifyToken);
-
-// Create a new group
 router.post('/', createGroup);
-
-// Get all groups with optional filtering
 router.get('/', getGroups);
-
-// Get a single group by ID
 router.get('/:id', getGroupById);
-
-// Update a group
 router.put('/:id', updateGroup);
-
-// Delete a group
 router.delete('/:id', deleteGroup);
 
 module.exports = router;
